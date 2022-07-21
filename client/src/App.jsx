0d9a634d4ddcf4f007a941b80574596c1a61db91
grantaspetraitis <grantas157@gmail.com>
2022-07-21 15:53:16 +0300

    navbar styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import AppContextProvider from './Context';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <AppContextProvider>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </AppContextProvider>
   );
 }
 
